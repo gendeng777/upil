568d6724792d54561b6cc4d609d15b3628533f9c
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-08 15:03:43 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -22,7 +22,7 @@ numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13"
 
 def get_one_random_kode(cod):
         cod = ('.txt')
-        for line in open(codlist,"r").readlines():
+        for line in open(cod,"r").readlines():
                 cod.append(line.strip())
 
 def get_one_random_number(nom):
