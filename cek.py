201fc849e63128a7220cbda53ff5e465ad656706
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-08 15:02:44 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -20,8 +20,8 @@ nom = []
 
 numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
 
-def get_one_random_kode(codlist):
-        codlist = ('.txt')
+def get_one_random_kode(cod):
+        cod = ('.txt')
         for line in open(codlist,"r").readlines():
                 cod.append(line.strip())
 
@@ -31,8 +31,8 @@ def get_one_random_number(nom):
 def cek():
     for i in range(0,1):
          one_number = str(get_one_random_number(nom))
-         one_kode = str(get_one_random_kode(codlist))         
-         print(nom + " => "  + codlist)
+         one_kode = str(get_one_random_kode(cod))         
+         print(nom + " => "  + cod)
 
 def main():                
     cek()
