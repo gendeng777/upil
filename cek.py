6bbf11bcc32e4be50af63c7a32962f46528e3bf5
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-08 14:47:27 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -27,32 +27,32 @@ nom = []
 
 numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
 
-def get_one_random_domain(domains):
-        return domains[random.randint( 0, len(domains)-1)]
+def get_one_random_kode(codlist):
+        codlist = ('.txt')
+                for line in open(codlist,"r").readlines():
+                        cod.append(line.strip())
 
-def get_one_random_number(numer):
-        return numer[random.randint(0, len(numer)-1)]
+def get_one_random_number(nom):
+        return nom[random.randint(0, len(nom)-1)]
 
-def em():
-    for i in range(0,50):
-         one_number = str(get_one_random_number(numer))
-         one_domain = str(get_one_random_domain(domains))         
-         print(nama + one_number  + "@" + one_domain)
+def cek():
+    for i in range(0,1):
+         one_number = str(get_one_random_number(nom))
+         one_code = str(get_one_random_code(codlist))         
+         print(nom + " => "  + codlist)
 
 def main():                
-    randomemails()
+    cek()
 
 os.system('clear')
 os.system('figlet CEK FB')
 nom = raw_input("\033[1;93mNOMOR HP \033[1;97m: ")
-codlist = ('.txt')
-	for line in open(codlist,"r").readlines():
-		cod.append(line.strip())
-except IOError:
-	print ("[!] File Not Found")
-	raw_input("\n[ Back ]")
-	menu()
-
+time.sleep(2)
+print"\033[1;97mCek kode FB",nom
+time.sleep(2)
+print"\033[1;93mSukses Vertifikasi Code FB"
+time.sleep(2)
+print"\033[1;96mKode : "+codlist
 
 if __name__=='__main__':
 	main()
