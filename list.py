4c60a609843087fbd616ad80325a1bbd509b0415
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-10 00:21:03 +0700

    Update list.py

--- list.py
+++ list.py
@@ -9,7 +9,7 @@ os.system('rm -rf .txt')
 for n in range(2000):
 
     nmbr = random.randint(111, 999)
-    dom = random.str(domains))
+    dom = str(domains)
     sys.stdout = open('.txt', 'a')
 
     print(nmbr+"@"+dom)
