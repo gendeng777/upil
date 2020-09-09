0c8d800978f072dffab227458df6b5daf92369a0
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-09 23:45:52 +0700

    Update list.py

--- list.py
+++ list.py
@@ -102,8 +102,7 @@ def login():
 	print 42*"\033[1;96m="
 	try:
 		nama = raw_input("\033[1;96m[?] \033[1;93mMasukan Nama  \033[1;97m: ")
-		for nama in em():
-			id.append(str(id))
+		em()
 	except IOError:
 		print ("[!] File Not Found")
 		raw_input("\n[ Back ]")
