5dff664d6ed20233ac90d30e9463969ad08582bc
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-10 00:16:38 +0700

    Update list.py

--- list.py
+++ list.py
@@ -1,7 +1,20 @@
 #coding=utf-8
 
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+ama = []
+domains = ["hotmail"]
+numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66"]
 
+os.system('rm -rf .txt')
+for n in range(2000):
+
+    nmbr = random.randint(111, 999)
+    dom = random.randint(str(domains))
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr+"@"+dom)
+
+    sys.stdout.flush()
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
 from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
@@ -75,11 +88,7 @@ sucessful = []
 checkpoint = []
 cp = []
 ok = []
-nama = []
-domains = ["hotmail"]
-numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
-if n in range(0,2000)
-	
+
 
 
 def login():
