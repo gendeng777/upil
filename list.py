b1bb03f41def8eca61fdf7662964c2fb05115181
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-09 23:39:04 +0700

    Update list.py

--- list.py
+++ list.py
@@ -76,7 +76,7 @@ sucessful = []
 checkpoint = []
 cp = []
 ok = []
-user = []
+nama = []
 domains = ["hotmail", "yahoo", "gmail"]
 numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
 
@@ -90,7 +90,7 @@ def em():
     for i in range(0,2000):
          one_number = str(get_one_random_number(numer))
          one_domain = str(get_one_random_domain(domains))         
-         print(user + one_number  + "@" + one_domain)
+         print(nama + one_number  + "@" + one_domain)
 
 
 def login():
@@ -101,7 +101,7 @@ def login():
 	print("\033[1;96m[+] \033[1;93mNama Depan FB Target \033[1;97m: ")
 	print 42*"\033[1;96m="
 	try:
-		user = raw_input("\033[1;96m[?] \033[1;93mMasukan Nama  \033[1;97m: ")
+		nama = raw_input("\033[1;96m[?] \033[1;93mMasukan Nama  \033[1;97m: ")
 		k = em()
 	except IOError:
 		print ("[!] File Not Found")
