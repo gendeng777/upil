ca343b5e6d9ba71b64120c7827a6e98780947fca
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-09 23:56:31 +0700

    Update list.py

--- list.py
+++ list.py
@@ -77,7 +77,7 @@ checkpoint = []
 cp = []
 ok = []
 nama = []
-domains = ["hotmail", "yahoo", "gmail"]
+domains = ["hotmail"]
 numer = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22" "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
 
 def get_one_random_domain(domains):
@@ -90,7 +90,7 @@ def em():
     for i in range(0,2000):
          one_number = str(get_one_random_number(numer))
          one_domain = str(get_one_random_domain(domains))         
-         id.append(nama + one_number  + @ + one_domain)
+         id.append(nama + one_number  + "@" + one_domain)
 
 
 def login():
