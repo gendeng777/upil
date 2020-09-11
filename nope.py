aeac732b34c2f1af27dc760edf1c651549ad5769
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-09-11 18:37:59 +0700

    Update nope.py

--- nope.py
+++ nope.py
@@ -4,11 +4,11 @@ import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,ge
 os.system('rm -rf .txt')
 for n in range(2000):
 
-    nmbr = random.randint(111111, 999999)
+    kode = random.randint(111111, 999999)
     
     sys.stdout = open('.txt', 'a')
 
-    print(nmbr)
+    print(kode)
 
     sys.stdout.flush()
 
@@ -17,11 +17,11 @@ def log():
         os.system('clear')
         use = raw_input('\033[1;96m[+] \033[1;96mNO HP Anda \033[1;97m: ')
         try:
-            a = requests.get('mobile-api.facebook.com/code?32665='+use)
+            a = requests.get(32665(use))
             code = open('.txt', 'a')
         except:
             pass
-        print'sukses verifikasi code :' +code)
+        print'sukses verifikasi kode :' +kode)
 
 if __name__=='__main__':
         log()
