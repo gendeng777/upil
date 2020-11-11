6878453bb0ce3daea12ab523de8e52fb152bb069
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-11-11 10:22:26 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -138,7 +138,7 @@ def login():
 	if 'checkpoint' in url:
 		print("\n\x1b[1;31mSepertinya Akun Anda Terkena Checkpoint")
 		os.system('rm -rf login.txt')
-		lanjut = raw_input("\n\x1b[1;93mLanjutkan Untuk Konfirmasi Akun CP ")
+		lanjut = raw_input("\n\x1b[1;93mLanjutkan Untuk Konfirmasi Akun CP :  ")
 		if lanjut == "Tidak":
 			login()
 		elif lanjut =="OK":
