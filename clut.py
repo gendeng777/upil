89cd9583b48535c4bcb3c34d0281ff41ede08cfe
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-11-24 09:03:20 +0700

    Update clut.py

--- clut.py
+++ clut.py
@@ -287,6 +287,7 @@ def crack_nam():
     print "\033[1;96m    \033[1;93m◆\033[1;31m ⃟ ⃟ \033[1;34m░▒▓ \033[1;93mCRACK USERNAME FB \033[1;34m▓▒░\033[1;31m ⃟ ⃟ \033[1;93m◆     \033[1;96m"
     print 42*"\033[1;96m="
     try:
+            print
             print '\033[1;96m[+] \033[1;93mContoh : \033[1;97mupil. \033[1;31mupil.pilek. \033[1;96dll'
             c = raw_input('\033[1;96m[+] \x1b[1;34mUsername : \033[1;31m ')
             print '\033[1;96m[+] \033[1;93mContoh : \033[1;96mupil123'
@@ -1293,6 +1294,7 @@ def crack_email():
     print "\033[1;96m    \033[1;93m◆\033[1;31m ⃟ ⃟ \033[1;34m░▒▓ \033[1;93mCRACK EMAIL FB \033[1;34m▓▒░\033[1;31m ⃟ ⃟ \033[1;93m◆     \033[1;96m"
     print 42*"\033[1;96m="
     try:
+        print
         print ('\033[1;96m[+] \033[1;93mContoh : \033[1;97mupil \033[1;93mupil_ \033[1;31mupil.pilek \033[1;96dll')
         c = raw_input('\033[1;96m[+] \x1b[1;34mNama Target : \033[1;31m ')
         print '\033[1;96m[+] \033[1;93mContoh : \033[1;31m@yahoo.com,\033[1;34@gmail.com \033[1;96dll'
